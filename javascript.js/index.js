1deb0d5343eef87ed8cc6ffe10258bbb36623900
DelicateM <delicatematsilele@gmail.com>
2024-11-04 21:39:23 +0200

    updated js

--- javascript.js/index.js
+++ javascript.js/index.js
@@ -7,8 +7,10 @@ function displayPoem(response) {
     cursor: "",
   });
 }
+
 function generatePoem(event) {
   event.preventDefault();
+
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "e3b3095tf9493f408498of3c2e1c6a18";
 
@@ -16,9 +18,11 @@ function generatePoem(event) {
     "You are a romantic poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML and separate each line with <br/>. Make sure to follow the user instructions.Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning.";
   let prompt = `User instructions:Generate a poem about ${instructionsInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
   console.log("Generating poem");
   console.log(`Prompt:${prompt}`);
   console.log(`Context:${context}`);
+
   axios.get(apiUrl).then(displayPoem);
 }
 let poemFormElement = document.querySelector("#poem-generator-form");
